1f7d9b07f7fdae61bb5c310f481bfb16ab895ce1
Tanisha744 <121763339+Tanisha744@users.noreply.github.com>
2024-03-15 11:57:25 +0530

    Add files via upload

---
+++ Project Fee/Project Code/main.js
@@ -0,0 +1,19 @@
+//! HAMBURGER
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+        console.log("nav-togle is clicked");
+    })
+}
+
+/* Menu hidden */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
